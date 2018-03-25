8d53188752be1c0bc1b2441c14f3d0d4b5a0ae18
Lloyd Trigg <lloyd.trigg@gmail.com>
2018-03-25 22:43:22 +1100

    test

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@
 const logger = require("../utils/logging");
 const { Pool } = require('pg');
 const dbPool = new Pool({
-	connectionString: process.env.DATABASE_URI,
-	ssl: ((process.env.DATABASE_SSL === 'true') || false)
+	connectionString: "postgres://master:hvdm8HZ2h@messagegate-poc.cmt7uglree8b.ap-southeast-2.rds.amazonaws.com:5432/messagegatepoc",
+	ssl: true
 });
 
 dbPool.on('error', (err, client) => {
