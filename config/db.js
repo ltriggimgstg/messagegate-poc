b6026846164ab83961b9e0bf5ac18f167ce27e61
Lloyd Trigg <lloyd.trigg@gmail.com>
2018-03-26 14:42:34 +1100

    db fixes

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ const logger = require("../utils/logging");
 const { Pool } = require('pg');
 const dbPool = new Pool({
 	connectionString: process.env.DATABASE_URI,
-	ssl: ((process.env.DATABASE_SSL === 'true') || false)
+	ssl: ((process.env.DATABASE_SSL === 'true') || false),
+	connectionTimeoutMillis: 5000,
+	idleTimeoutMillis: 10000,
+	max: 10
 });
 
 dbPool.on('error', (err, client) => {
--- handlers/sms.js
+++ handlers/sms.js
@@ -7,18 +7,14 @@ const base= require("./base");
 module.exports = {
 	get: (evt, ctx, cb) => {
 		logger.debug("handler/sms.get");
-		return new Promise((resolve, reject) => {
-            resolve(new Sms().get());
-        })
+		return new Sms().get()
 		.then(data => base.responder(data))
 		.then(data => cb(data.error, data.response))
 		.catch(err => cb(err));
 	},
 	post: (evt, ctx, cb) => {
 		logger.debug("handler/sms.post");
-		return new Promise((resolve, reject) => {
-            resolve(new Sms().get());
-        })
+		return new Sms().get()
 		.then(data => base.responder(data))
 		.then(data => cb(data.error, data.response))
 		.catch(err => cb(err));
